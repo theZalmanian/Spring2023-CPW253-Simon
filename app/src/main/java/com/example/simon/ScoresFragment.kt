8b481dd1e036dd97fdab691f43dd00fbc1fbed32
theZalmanian <thezalmanian@gmail.com>
2023-06-12 05:19:29 -0700

    Previous game scores are now displayed on the ScoresFragment.kt - Added item_score.xml to format gameData display - Implemented all necessary DB and ListView code in ScoresFragment.kt

--- app/src/main/java/com/example/simon/ScoresFragment.kt
+++ app/src/main/java/com/example/simon/ScoresFragment.kt
@@ -5,11 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import com.example.simon.databinding.FragmentScoresBinding
 
 class ScoresFragment : Fragment() {
+    private lateinit var binding: FragmentScoresBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scores, container, false)
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout using view binding
+        binding = FragmentScoresBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // get access to the SimonGameDao from the database instance
+        val simonGameDao = SimonGameDatabase.getInstance(requireContext()).simonGameDao
+
+        // get the ListView used to display the data for each game
+        val lstScoresDisplay: ListView = binding.lstScoresDisplay
+
+        // get all the games stored in the DB
+        simonGameDao.getAllGames().observe(viewLifecycleOwner) { gameDataList ->
+            // display them all on the "Scores Display" ListView
+            lstScoresDisplay.adapter = ScoresAdapter(requireContext(), gameDataList)
+        }
     }
 }
