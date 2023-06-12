f441fbf4829c02b324f890a73d8b6aca0b1a1298
theZalmanian <thezalmanian@gmail.com>
2023-06-11 22:28:09 -0700

    Updated dependencies and modified the VM Factory to generate the correct class

--- app/src/main/java/com/example/simon/SimonGameViewModelFactory.kt
+++ app/src/main/java/com/example/simon/SimonGameViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 
 class SimonGameViewModelFactory(private val dao:SimonGameDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(SimonGame::class.java)) {
+        if(modelClass.isAssignableFrom(SimonGameViewModel::class.java)) {
             return SimonGameViewModel(dao) as T
         }
         throw IllegalArgumentException("Unknown View Model")
